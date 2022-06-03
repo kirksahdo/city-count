37624234b47fbc91abc52dd5b18d50ecd1fdcd55
KIRK SAHDO <ksahdo10@gmail.com>
2022-06-03 00:05:09 -0400

    feat: add route file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,14 @@
-export { };
+import {
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={ <p>Initial Page</p> } />
+      <Route path="*" element={ <Navigate to="/" /> } />
+    </Routes>
+  );
+}
