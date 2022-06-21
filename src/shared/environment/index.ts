ed2408dc2790a233171a2b60b562b208dd79f348
KIRK SAHDO <ksahdo10@gmail.com>
2022-06-20 21:44:56 -0400

    feat: add enviroment variables

---
+++ src/shared/environment/index.ts
@@ -0,0 +1,22 @@
+export const Enviroment = {
+  /**
+   * Defines the lines length that will be loaded by default on listing
+   */
+  LINES_LIMIT: 10,
+
+  /**
+   * Defines the search input default placeholder
+   */
+  SEARCH_INPUT: 'Search..',
+
+  /**
+   * Defines the message when register not found on listing
+   */
+  EMPTY_LIST: 'Register not found.',
+
+  /**
+   * Defines url base from back-end server
+   */
+  URL_BASE: 'http://localhost:3333',
+
+};
