362bd7f181ca66ad2ef41e7d84f170374d10fb7a
KIRK SAHDO <ksahdo10@gmail.com>
2022-06-24 22:05:11 -0400

    feat: add users list

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,22 +1,34 @@
-import { useMemo } from 'react';
+import { useMemo, useEffect, useCallback, useState} from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { ListingBar } from '../../shared/components';
 import { BasePageLayout } from '../../shared/layouts';
+import { TUserWithTotalCount } from '../../shared/services/api/users/interfaces';
+import { userService } from '../../shared/services/api/users/UsersService';
 
 export const Users: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [users, setUsers] = useState<TUserWithTotalCount>({} as TUserWithTotalCount);
 
-  const handleSearch = (searchText: string) => {
+  const handleSearch = useCallback((searchText: string) => {
     const params = {
       search: searchText,
     };
     setSearchParams(params);
-  };
+  }, []);
 
   const searchText = useMemo(() => {
     return searchParams.get('search') || '';
   }, [searchParams]);
+
+  useEffect(() => {
+    userService.getAll(1, searchText).then(result => {
+      if (result instanceof Error) {
+        return alert(result.message);
+      }
+      setUsers(result);
+    });
+  }, [searchText]);
 
   return (
     <BasePageLayout
